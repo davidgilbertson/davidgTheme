e01f773af0164f81c28aa96a353c3ca08b7d303c
David Gilbertson <gilbertson.david@gmail.com>
2014-04-15 07:33:12 +1000

    Home page looking good (enough)
    CSS is a mess.

--- content-page.php
+++ content-page.php
@@ -1,5 +1,5 @@
 <?php //This serves as the default template ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class('col-xs-12'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('home-article'); ?>>
 	<div class="entry-content">
 		<?php the_content(); ?>
 	</div>
--- header.php
+++ header.php
@@ -24,14 +24,24 @@
   <![endif]-->
   <script>var DG = {};</script>
   <?php wp_head(); ?>
-  <link rel="prefetch" href="blog" />
+
+  <!-- <link rel="prefetch" href="blog" /> -->
   <!-- <link rel="prerender" href="blog"> -->
+
+  <!-- <link href='http://fonts.googleapis.com/css?family=Lora' rel='stylesheet' type='text/css'> -->
+  <link href='http://fonts.googleapis.com/css?family=Josefin+Slab:300,400,600' rel='stylesheet' type='text/css'>
   <style>
     @font-face {
         font-family: 'Raleway';
         font-style: normal;
         font-weight: 300;
         src: local('Raleway Light'), local('Raleway-Light'), url(http://themes.googleusercontent.com/static/fonts/raleway/v7/-_Ctzj9b56b8RgXW8FAriRsxEYwM7FgeyaSgU71cLG0.woff) format('woff');
+    }
+    @font-face {
+        font-family: 'Lora';
+        font-style: normal;
+        font-weight: 200;
+        src: local('Lora'), local('Lora'), url(http://themes.googleusercontent.com/static/fonts/raleway/v7/-_Ctzj9b56b8RgXW8FAriRsxEYwM7FgeyaSgU71cLG0.woffwoff) format('woff');
     }
     </style>
 </head>
